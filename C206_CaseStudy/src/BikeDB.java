61ed1b1285e78841dab2c1a52ae25054d4413079
RASBASHA <RASBASHA@Muhsin>
2020-08-11 15:50:08 +0800

    First Push

---
+++ C206_CaseStudy/src/BikeDB.java
@@ -0,0 +1,15 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * RASBASHA, 11 Aug 2020 3:33:04 pm
+ */
+
+/**
+ * @author Muhsin
+ *
+ */
+public class BikeDB {
+
+}
