1761bbe887ac01883ce645b1977a62d8ca2d9ea6
19028690 <19028690@CHADS-LAPTOP>
2020-08-13 14:09:41 +0800

    Skeleton Bike and BikeDB

--- C206_CaseStudy/src/Bike.java
+++ C206_CaseStudy/src/Bike.java
@@ -1,15 +1,39 @@
-/**
- * I declare that this code was written by me.
- * I will not copy or allow others to copy my code.
- * I understand that copying code is considered as plagiarism.
- *
- * RASBASHA, 11 Aug 2020 3:33:32 pm
- */
 
-/**
- * @author Muhsin
- *
- */
 public class Bike {
+
+	// declare class parameters
+
+	public Bike(String model, String description, boolean isAvailable) {
+		// initialize paramaters
+	}
+
+	public String getModel() {
+
+	}
+
+	public void setModel(String model) {
+
+	}
+
+	public String getDescription() {
+
+	}
+
+	public void setDescription(String description) {
+
+	}
+
+	public boolean getIsAvailable() {
+
+	}
+
+	public void setIsAvailable(boolean isAvailable) {
+
+	}
+
+	public String toString() {
+		// format string to return
+
+	}
 
 }
--- C206_CaseStudy/src/BikeDB.java
+++ C206_CaseStudy/src/BikeDB.java
@@ -1,15 +1,87 @@
-/**
- * I declare that this code was written by me.
- * I will not copy or allow others to copy my code.
- * I understand that copying code is considered as plagiarism.
- *
- * Muhsin, 11 Aug 2020 3:33:04 pm
- */
+import java.util.ArrayList;
 
-/**
- * @author Muhsin
- *
- */
 public class BikeDB {
 
+	private static ArrayList<Bike> bikeList = new ArrayList<Bike>();
+
+	public static void populateBikeDB() {
+		// add some data to the list
+
+	}
+
+	public static void cleanupBikeDB() {
+		// clean up the data in the list
+
+	}
+
+	public static void handleBikeOptions() {
+		// show bike menu, ask user for input, process the choice
+		int subOption = 0;
+
+	}
+
+	public static void showBikeMenu() {
+		// Display the menu
+	}
+
+	public static void processOption(int subOption) {
+
+		while (subOption != 4) {
+			if (subOption == 1) {
+				// View all bike
+
+			} else if (subOption == 2) {
+				// Add a bike
+
+			} else if (subOption == 3) {
+				// Delete a bike
+
+			} else {
+				// invalid option
+				System.out.println("Invalid type");
+			}
+
+			// show the menu again & ask for option
+
+		}
+
+	}
+
+	// ==========Option 1 ==============
+	public static void viewAllBike() {
+		// display all bikes in the list
+	}
+
+	public static String retrieveAllBike() {
+		// retrieve and format all bikes in the list
+	}
+
+	// ==========Option 2 ==============
+	public static Bike inputBikeDetails() {
+		// request user for the bike to add to the list
+	}
+
+	public static void addBike(Bike bk) {
+		// add bike to list
+
+	}
+
+	// ==========Option 3 ==============
+	public static int selectBike() {
+		// request the user to select bike for process
+	}
+
+	public static void delBike(int bikeID) {
+		// remove bike to list
+
+	}
+
+	// ==============Helper Functions=======
+	public static int getBikeDBSize() {
+		return bikeList.size();
+	}
+
+	public static String getBikeInfo(int bikeID) {
+		// format and return the string
+	}
 }
