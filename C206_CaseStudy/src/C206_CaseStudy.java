2bb3117158ac94162764685e5ea4faeae51c5e86
19028690 <19028690@CHADS-LAPTOP>
2020-08-13 13:58:38 +0800

    C206 Skeleton Template with Appointment Menu Added

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,58 @@
 
 public class C206_CaseStudy {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static final int OPTION_QUIT = 5;
 
+	public static void main(String[] args) {
+
+		// insert initial data
+
+//		BikeDB.populateBikeDB();
+
+		AppointmentDB.populateAppointmentDB(); // Create by Chad on 10/08/2020
+
+		int option = 0;
+
+		while (option != OPTION_QUIT) {
+
+			C206_CaseStudy.showMainMenu();
+			option = Helper.readInt("Enter an option > ");
+
+			if (option == 1) {
+				// Call BikeDB
+//				BikeDB.handleBikeOptions();
+			} else if (option == 2) {
+				// Call BikePartDB menu
+
+			} else if (option == 3) {
+				// Call Feedback menu
+
+			} else if (option == 4) {
+				// Call Appointment menu
+				AppointmentDB.handleAppointmentOptions(); // Created by Chad on 10/08/2020
+			} else if (option == 5) {
+				// quit
+				System.out.println("Program Terminated"); // Created by Chad on 10/08/2020
+			}
+
+		}
+
+		// clean up data
+//		BikeDB.cleanupBikeDB();
+
+		AppointmentDB.cleanupAppointmentDB(); // Created by Chad on 10/08/2020
+
+	}
+
+	public static void showMainMenu() { // Created by Chad on 10/08/2020
+		// insert your menu here
+		Helper.line(50, "=");
+		System.out.println("BIKE LOVER'S COMMUNITY MANAGER");
+		Helper.line(50, "=");
+		System.out.println("1. Bike Menu");
+		System.out.println("2. Bike Part Menu");
+		System.out.println("3. Feedback Menu");
+		System.out.println("4. Appointment Menu");
 	}
 
 }
