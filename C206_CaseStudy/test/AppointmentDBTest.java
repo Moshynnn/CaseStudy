3705579e4de98e7cb832043b20c4aa50babd6368
19028690 <19028690@172.24.16.51>
2020-08-17 07:36:52 +0800

    Update methods using setAppointmentArrayList and getAppointmentArrayList to setAppointmentList and getAppointmentList accordingly

--- C206_CaseStudy/test/AppointmentDBTest.java
+++ C206_CaseStudy/test/AppointmentDBTest.java
@@ -23,52 +23,49 @@ public class AppointmentDBTest {
 		// prepare test data
 		ap1 = new Appointment("John", "11/08/2020", "22:08");
 		ap2 = new Appointment("James", "15/08/2020", "23:08");
-		AppointmentDB.setAppointmentArrayList(new ArrayList<Appointment>());
+		AppointmentDB.setAppointmentList(new ArrayList<Appointment>());
 	}
 
 	@Test
 	public void retrieveAllAppointment() {
 		// Appointment list is not null, so that can add a new appointment - boundary
-		assertNotNull("Check if there is valid Appointment arraylist to add to",
-				AppointmentDB.getAppointmentArrayList());
+		assertNotNull("Check if there is valid Appointment arraylist to add to", AppointmentDB.getAppointmentList());
 		AppointmentDB.addAppointment(ap1);
 		AppointmentDB.addAppointment(ap2);
-		assertEquals("Check that Appointment arraylist size is 2", 2, AppointmentDB.getAppointmentArrayList().size());
+		assertEquals("Check that Appointment arraylist size is 2", 2, AppointmentDB.getAppointmentList().size());
 
 	}
 
 	@Test
 	public void addAppointmentTest() {
 		// Appointment list is not null, so that can add a new appointment - boundary
-		assertNotNull("Check if there is valid Appointment arraylist to add to",
-				AppointmentDB.getAppointmentArrayList());
+		assertNotNull("Check if there is valid Appointment arraylist to add to", AppointmentDB.getAppointmentList());
 		// After appointment is added Appointment list size should increase - normal
 		AppointmentDB.addAppointment(ap1);
-		assertEquals("Check that Appointment arraylist size is 1", 1, AppointmentDB.getAppointmentArrayList().size());
+		assertEquals("Check that Appointment arraylist size is 1", 1, AppointmentDB.getAppointmentList().size());
 		// Then check if correct Appointment was added
-		assertSame("Check that Appointment is added", ap1, AppointmentDB.getAppointmentArrayList().get(0));
+		assertSame("Check that Appointment is added", ap1, AppointmentDB.getAppointmentList().get(0));
 	}
 
 	@Test
 	public void delAppointmentTest() {
 		// Appointment list is not null, so that can add a new appointments - boundary
-		assertNotNull("Check if there is valid Appointment arraylist to add to",
-				AppointmentDB.getAppointmentArrayList());
+		assertNotNull("Check if there is valid Appointment arraylist to add to", AppointmentDB.getAppointmentList());
 		AppointmentDB.addAppointment(ap1);
 		AppointmentDB.addAppointment(ap2);
 		// Given there are two appointments in the Appointment list when one appointment
 		// is deleted than the Appointment list size should decrease to one
 		AppointmentDB.delAppointment(ap1);
-		assertEquals("Check that Appointment arraylist size is 1", 1, AppointmentDB.getAppointmentArrayList().size());
+		assertEquals("Check that Appointment arraylist size is 1", 1, AppointmentDB.getAppointmentList().size());
 		// Then check if the correct Appointment was deleted
-		assertSame("Check that Appointment is added", ap2, AppointmentDB.getAppointmentArrayList().get(0));
+		assertSame("Check that Appointment is added", ap2, AppointmentDB.getAppointmentList().get(0));
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		ap1 = null;
 		ap2 = null;
-		AppointmentDB.setAppointmentArrayList(null);
+		AppointmentDB.setAppointmentList(null);
 	}
 
 }
