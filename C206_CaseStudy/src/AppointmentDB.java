97252239e00619970a76f912ea506de3feb6cfc4
19028690 <19028690@CHADS-LAPTOP>
2020-08-20 08:36:30 +0800

    Added search option for AppointmentDB

--- C206_CaseStudy/src/AppointmentDB.java
+++ C206_CaseStudy/src/AppointmentDB.java
@@ -38,11 +38,12 @@ public class AppointmentDB {
 		System.out.println("1. View All Appointment");
 		System.out.println("2. Add Appointment");
 		System.out.println("3. Delete Appointment");
-		System.out.println("4. Back");
+		System.out.println("4. Search Appointment");
+		System.out.println("5. Back");
 	}
 
 	public static void processOption(int subOption) {
-		while (subOption != 4) {
+		while (subOption != 5) {
 			showAppointmentMenu();
 			subOption = Helper.readInt("Enter Option > ");
 			if (subOption == 1) {
@@ -61,6 +62,8 @@ public class AppointmentDB {
 					System.out.println(e.getMessage());
 				}
 			} else if (subOption == 4) {
+				searchAppointment();
+			} else if (subOption == 5) {
 				System.out.println("Appointment Menu Closed");
 			} else {
 				System.out.println("Invalid Option");
@@ -112,6 +115,32 @@ public class AppointmentDB {
 	public static void delAppointment(Appointment a) {
 		appointmentList.remove(a);
 		System.out.println("Appointment deleted");
+	}
+
+	// ========== Option 4 ==============
+	public static void searchAppointment() {
+		boolean hasCustomerName = false;
+		String output = "";
+		int i = 0;
+		if (!appointmentList.isEmpty()) {
+			output = String.format("%-5S%-15S%-15S%S\n", "ID", "NAME", "DATE", "TIME");
+			String name = Helper.readString("Enter Customer Name > ");
+			for (Appointment a : appointmentList) {
+				if (a.getCustomerName().equalsIgnoreCase(name)) {
+					i++;
+					output += String.format("%-5S%-15S%-15S%S\n", i, a.getCustomerName(), a.getDate(), a.getTime());
+					hasCustomerName = true;
+				}
+			}
+		} else {
+			output += "No Appointments in the Database";
+		}
+		if (hasCustomerName) {
+			System.out.println(output);
+		} else {
+			System.out.println("There are no appointments under that name");
+		}
+
 	}
 
 	// ============== Helper Functions=======
